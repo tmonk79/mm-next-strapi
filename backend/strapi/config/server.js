c7db9380dfdac3d798bd2b0a819858ad07370cad
Pankaj Shere <pankaj.shere@mediamonks.com>
2022-02-14 20:09:40 +0530

    dockerized strapi with vol mappings

---
+++ backend/strapi/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '389fcb74308d4ec49828ac67f549ae5e'),
+    },
+  },
+});
---
+++ backend/strapi/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '19ee6f17-93db-474c-ade5-c642f4cb9286'
+};
