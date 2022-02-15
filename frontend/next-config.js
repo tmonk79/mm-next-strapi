4a2753769cf4824d00683fa359883935c5c8388a
Pradeep <pradeep@Pradeeps-MacBook-Pro.local>
2022-02-15 15:57:00 +0530

    Updated the frontend codebase

---
+++ frontend/next-config.js
@@ -0,0 +1,12 @@
+const { PHASE_DEVELOPMENT_SERVER } = require('next/constants');
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      env: {},
+    };
+  }
+  return {
+    env: {},
+  };
+};
