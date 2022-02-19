7982f27529d99e25972dd8d0cd177db743b82f0d
Pankaj Shere <pankaj.shere@mediamonks.com>
2022-02-19 10:32:43 +0530

    nginx proxy working

--- backend/strapi/config/server.js
+++ backend/strapi/config/server.js
@@ -1,9 +1,11 @@
 module.exports = ({ env }) => ({
   host: process.env.STRAPI_HOST,
   port: process.env.STRAPI_PORT,
+  url: 'https://dessertcorner.com',
   admin: {
     auth: {
       secret: process.env.JWT_SECRET,
     },
+    url: '/admin'
   },
 });
